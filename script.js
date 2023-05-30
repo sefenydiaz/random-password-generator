7826ffce3aac981e0aef1ec0fd58da02e606219b
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-05-30 17:26:43 -0400

    fixed js code to generate password

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 
 
 
-var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
 
-var generatePassword = [];
-var validCharacters = [];
-//use .split method we learned in class !!!
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('') ;
-var lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('') ;
-var numbers = '1234567890'.split('') ;
-var specialCharacters = '!@#$%^&*'.split('');
-// need arrays for .push
-var uppercaseArray = uppercase;
-var lowercaseArray = lowercase;
-var numbersArray = numbers;
-var specialCharactersArray = specialCharacters;
+  var generatePassword = [];
+  var validCharacters = [];
+  //use .split method we learned in class !!!
+  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  var numbers = '1234567890';
+  var specialCharacters = '!@#$%^&*';
+  // need arrays for .push
+  var uppercaseArray = uppercase.split('');
+  var lowercaseArray = lowercase.split('');
+  var numbersArray = numbers.split('');
+  var specialCharactersArray = specialCharacters.split('');
 
-// FUNCTIONS
+  // FUNCTIONS
   // write password prompt
   //password can only be 8-128 characters
   //prompt: do you want uppercase letters?
@@ -27,43 +27,46 @@ var specialCharactersArray = specialCharacters;
   //prompt: do you want special characters
   // generate random password
   //alert: here is your random password!
-var passwordLength = prompt("What is your desired password length? At least 8 characters but no longer than 128.");
-var includeUppercase = confirm("Would you like to include lowercase characters?");
-var includeLowercase = confirm("Would you like to include uppercase characters?");
-var includeNumbers = confirm("Would you like to include numbers?");
-var includespecialCharacters = confirm("Would you like to include special characters?");
+  var passwordLength = prompt("What is your desired password length? At least 8 characters but no longer than 128.");
+  var includeUppercase = confirm("Would you like to include lowercase characters?");
+  var includeLowercase = confirm("Would you like to include uppercase characters?");
+  var includeNumbers = confirm("Would you like to include numbers?");
+  var includespecialCharacters = confirm("Would you like to include special characters?");
 
-console.log(passwordLength)
+  // checking to make sure password length is correct
+  console.log(passwordLength)
 
 
-//need if statement to keep password between 8-128 characters
-if (passwordLength < 8 || passwordLength > 128) {
-  passwordLength = prompt("You have entered an invalid password length. Please choose a number between 8 and 128.");
+  //need if statement to keep password between 8-128 characters
+  if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You have entered an invalid password length. Please choose a number between 8 and 128.");
+  }
+
+  //if statements for characters
+  if (includeLowercase) [
+    validCharacters.push(...lowercaseArray),
+  ]
+
+  if (includeUppercase) [
+    validCharacters.push(...uppercaseArray),
+  ]
+
+  if (includeNumbers) [
+    validCharacters.push(...numbersArray),
+  ]
+
+  if (includespecialCharacters) [
+    validCharacters.push(...specialCharactersArray),
+  ]
+
+  //the dreaded for loop...
+  for (var i = 0; i < passwordLength; i++) {
+    generatePassword.push((validCharacters[Math.floor(Math.random() * validCharacters.length)]));
+  }
+  return (generatePassword.join(''));
 }
 
-//if statements for characters
-if (includeLowercase) [
-  validCharacters.push(lowercaseArray) ,
-]
-
-if (includeUppercase) [
-  validCharacters.push(uppercaseArray) ,
-]
-
-if (includeNumbers) [
-  validCharacters.push(numbersArray) ,
-]
-
-if (includespecialCharacters) [
-  validCharacters.push(specialCharactersArray) ,
-]
-
-//the dreaded for loop...
-for(var i = 0; i < passwordLength; i++) {
-  generatePassword.push((validCharacters[Math.floor(Math.random() * validCharacters.length)]));
-}
-return(generatePassword.join(''));
-}
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -75,4 +78,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) ;
+generateBtn.addEventListener("click", writePassword);
